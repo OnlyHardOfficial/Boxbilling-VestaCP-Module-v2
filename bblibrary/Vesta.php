b476e78119150a10fc309eca84f823afb394d523
Ramael Metatron <ramael@yoga.sacred.network>
2017-01-17 17:10:41 -0600

    Disabled adding domain during account creation.

--- bblibrary/Vesta.php
+++ bblibrary/Vesta.php
@@ -20,15 +20,15 @@ class Server_Manager_Vesta extends Server_Manager
 {
     /**
      * Method is called just after obejct contruct is complete.
-     * Add required parameters checks here. 
+     * Add required parameters checks here.
      */
 	public function init()
     {
-        
+
 	}
     /**
      * Return server manager parameters.
-     * @return type 
+     * @return type
      */
     public static function getForm()
     {
@@ -38,8 +38,8 @@ class Server_Manager_Vesta extends Server_Manager
     }
     /**
      * Returns link to account management page
-     * 
-     * @return string 
+     *
+     * @return string
      */
     public function getLoginUrl()
     {
@@ -52,13 +52,13 @@ class Server_Manager_Vesta extends Server_Manager
     }
     /**
      * Returns link to reseller account management
-     * @return string 
+     * @return string
      */
     public function getResellerLoginUrl()
     {
         return 'http://www.google.com?q=whm';
     }
-   
+
 private function _makeRequest($params)
     {
 $host = 'http';
@@ -66,13 +66,13 @@ $host = 'http';
 			$host .= 's';
 		}
 		$host .= '://' . $this->_config['host'] . ':'.$this->_config['port'].'/api/';
-    	
-    	
+
+
 // Server credentials
 $params['user'] = $this->_config['username'];
 $params['password'] = $this->_config['password'];
-   	
-    	
+
+
 // Send POST query via cURL
 $postdata = http_build_query($params);
 $curl = curl_init();
@@ -89,38 +89,38 @@ curl_close($curl);
 		if(strpos($result, 'Error')!== false){
 throw new Server_Exception('Connection to server failed  '.$result);
     	}
-		
+
 			return $result;
     }
 private function _getPackageName(Server_Package $package)
     {
         $name = $package->getName();
-        
+
         return $name;
     }
     /**
      * This method is called to check if configuration is correct
      * and class can connect to server
-     * 
-     * @return boolean 
+     *
+     * @return boolean
      */
     public function testConnection()
     {
-      
-    
-           
+
+
+
         // Server credentials
 $vst_command = 'v-check-user-password';
 $vst_returncode = 'yes';
 // Prepare POST query
 $postvars = array(
-    
+
     'returncode' => $vst_returncode,
     'cmd' => $vst_command,
     'arg1' => $this->_config['username'],
     'arg2' => $this->_config['password'],
 );
-    
+
 // Make request and check sys info
 $result = $this->_makeRequest($postvars);
 if(strpos($result, 'Error')!== false){
@@ -139,7 +139,7 @@ if ($result == 0) {
      * MEthods retrieves information from server, assignes new values to
      * cloned Server_Account object and returns it.
      * @param Server_Account $a
-     * @return Server_Account 
+     * @return Server_Account
      */
     public function synchronizeAccount(Server_Account $a)
     {
@@ -151,16 +151,16 @@ if ($result == 0) {
     }
     /**
      * Create new account on server
-     * 
-     * @param Server_Account $a 
+     *
+     * @param Server_Account $a
      */
 	public function createAccount(Server_Account $a)
     {
-           
+
            $p = $a->getPackage();
            $packname = $this->_getPackageName($p);
-		
-		
+
+
 		$client = $a->getClient();
         // Server credentials
 $vst_command = 'v-add-user';
@@ -170,7 +170,7 @@ $lastname = array_pop($parts);
 $firstname = implode(" ", $parts);
 // Prepare POST query
 $postvars = array(
-    
+
     'returncode' => $vst_returncode,
     'cmd' => $vst_command,
     'arg1' => $a->getUsername(),
@@ -178,45 +178,45 @@ $postvars = array(
     'arg3' => $client->getEmail(),
     'arg4' => $packname,
     'arg5' => $firstname,
-    'arg6' => $lastname							
-);    
-// Make request and create user 
-$result = $this->_makeRequest($postvars);
-if($result == 0)
-{
-		
-// Create Domain Prepare POST query
-$postvars2 = array(
-    
-    'returncode' => 'yes',
-    'cmd' => 'v-add-domain',
-    'arg1' => $a->getUsername(),
-    'arg2' => $a->getDomain()
+    'arg6' => $lastname
 );
-$result2 = $this->_makeRequest($postvars2);
-}
-else {
-}
-if($result2 != '0'){
-throw new Server_Exception('Server Manager Vesta CP Error: Create Domain failure '.$result2);
-}
+// Make request and create user
+$result = $this->_makeRequest($postvars);
+// if($result == 0)
+// {
+//
+// // Create Domain Prepare POST query
+// $postvars2 = array(
+//
+//     'returncode' => 'yes',
+//     'cmd' => 'v-add-domain',
+//     'arg1' => $a->getUsername(),
+//     'arg2' => $a->getDomain()
+// );
+// $result2 = $this->_makeRequest($postvars2);
+// }
+// else {
+// }
+// if($result2 != '0'){
+// throw new Server_Exception('Server Manager Vesta CP Error: Create Domain failure '.$result2);
+// }
 	return true;
 	}
     /**
      * Suspend account on server
-     * @param Server_Account $a 
+     * @param Server_Account $a
      */
 	public function suspendAccount(Server_Account $a)
     {
 $user = $a->getUsername();
 // Prepare POST query
-$postvars = array(    
+$postvars = array(
     'returncode' => 'yes',
     'cmd' => 'v-suspend-user',
     'arg1' => $a->getUsername(),
-    'arg2' => 'no'  
-                  );    
-// Make request and suspend user 
+    'arg2' => 'no'
+                  );
+// Make request and suspend user
 $result = $this->_makeRequest($postvars);
 // Check if error 6 the account is suspended on server
 if($result == '6'){
@@ -226,21 +226,21 @@ if($result != '0'){
 throw new Server_Exception('Server Manager Vesta CP Error: Suspend Account Error '.$result.$suspended);
 }
 return true;
-        
+
 	}
     /**
      * Unsuspend account on server
-     * @param Server_Account $a 
+     * @param Server_Account $a
      */
 	public function unsuspendAccount(Server_Account $a)
     {
-           
+
         // Server credentials
 $vst_command = 'v-unsuspend-user';
 $vst_returncode = 'yes';
 // Prepare POST query
 $postvars = array(
-    
+
     'returncode' => $vst_returncode,
     'cmd' => $vst_command,
     'arg1' => $a->getUsername(),
@@ -252,24 +252,24 @@ $postvars = array(
     'arg7' =>'',
     'arg8' =>'',
     'arg9' =>''
-		
-);  
-  
+
+);
+
 		$result = $this->_makeRequest($postvars);
 if($result != '0'){
 throw new Server_Exception('Server Manager Vesta CP Error: unSuspend Account Error '.$result);
 }
 return true;
 }
-	
+
     /**
      * Cancel account on server
-     * @param Server_Account $a 
+     * @param Server_Account $a
      */
 	public function cancelAccount(Server_Account $a)
     {
-        
-           
+
+
         // Server credentials
 $vst_username = $this->_config['username'];
 $vst_password = $this->_config['password'];
@@ -277,14 +277,14 @@ $vst_command = 'v-delete-user';
 $vst_returncode = 'yes';
 // Prepare POST query
 $postvars = array(
-    
+
     'returncode' => $vst_returncode,
     'cmd' => $vst_command,
     'arg1' => $a->getUsername(),
     'arg2' => 'no'
-						
-);    
-// Make request and delete user 
+
+);
+// Make request and delete user
 		$result = $this->_makeRequest($postvars);
 if($result == '3'){
 return true;
@@ -297,14 +297,14 @@ return true;
     /**
      * Change account package on server
      * @param Server_Account $a
-     * @param Server_Package $p 
+     * @param Server_Package $p
      */
 	public function changeAccountPackage(Server_Account $a, Server_Package $p)
     {
-        
+
 $package = $a->getPackage()->getName();
-        
-        
+
+
 // Server credentials
 $vst_username = $this->_config['username'];
 $vst_password = $this->_config['password'];
@@ -312,14 +312,14 @@ $vst_command = 'v-change-user-package';
 $vst_returncode = 'yes';
 // Prepare POST query
 $postvars = array(
-    
+
     'returncode' => $vst_returncode,
     'cmd' => $vst_command,
     'arg1' => $a->getUsername(),
     'arg2' => $this->_getPackageName($p),
     'arg3' => 'no'
-			
-);    
+
+);
 // Make request and change package
 		$result = $this->_makeRequest($postvars);
 if($result != '0'){
@@ -356,8 +356,8 @@ throw new Server_Exception('Server Manager Vesta CP Error: Not Supported');
      */
     public function changeAccountPassword(Server_Account $a, $new)
     {
-        
-           
+
+
         // Server credentials
 $vst_username = $this->_config['username'];
 $vst_password = $this->_config['password'];
@@ -365,19 +365,19 @@ $vst_command = 'v-change-user-password';
 $vst_returncode = 'yes';
 // Prepare POST query
 $postvars = array(
-    
+
     'returncode' => $vst_returncode,
     'cmd' => $vst_command,
     'arg1' => $a->getUsername(),
     'arg2' => $new
-			
-);    
+
+);
 // Make request and change password
 $result = $this->_makeRequest($postvars);
 if($result != '0'){
 throw new Server_Exception('Server Manager Vesta CP Error: Change Password Account Error '.$result);
 }
-        
+
 return true;
     }
     /**
@@ -387,7 +387,7 @@ return true;
      */
     public function changeAccountIp(Server_Account $a, $new)
     {
-        
+
       {
 throw new Server_Exception('Server Manager Vesta CP Error: Not Supported');
 }
